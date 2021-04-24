7a47a04c74bf62f5cf5f6e6f763776f7463fc744
Kiara <kiarabroekhuizen1@hotmail.com>
2021-04-24 17:13:35 +0200

    started basic routing

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const app = express();
 const port = 1234;
 
 app.get('/', helloWorld);
+app.get('/about', about)
+app.get('/contact', contact)
+app.use(notFound)
+
+app.listen(port, function(){
+    console.log('The server is running at http://localhost:1234')
+    }
+);
 
 function helloWorld(req, res) {
     res.send('hola Mundo')
 }
 
-app.listen(port, function(){
-    console.log('The server is running')
-    }
-);
+function about(req,res) {
+res.send('this is about something')
+}
+
+function contact(req, res) {
+    res.send('normally you would get some contact info here...')
+}
+
+function notFound(req,res) {
+    res.status(404).send('something went wrong....')
+}
